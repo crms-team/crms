d60892013db159e17cf782d3d6148b401a3df6e4
Nevation <j209446@naver.com>
2020-08-12 22:20:14 +0900

    Create CRMS AWS EC2

--- crms/aws/compute/ec2.js
+++ crms/aws/compute/ec2.js
@@ -11,5 +11,63 @@ module.exports = {
         let ec2 = new AWS.EC2({ apiVersion: '2016-11-15' })
         return await ec2.describeInstanceTypes().promise()
     },
-
+    stopInstances: async (key, id) => {
+        AWS.config.update(key)
+        let ec2 = new AWS.EC2({ apiVersion: '2016-11-15' })
+        try {
+            await ec2.stopInstances({
+                InstanceIds: [id]
+            }).promise()
+            return true
+        } catch {
+            return false
+        }
+    },
+    startInstances: async (key, id) => {
+        AWS.config.update(key)
+        let ec2 = new AWS.EC2({ apiVersion: '2016-11-15' })
+        try {
+            await ec2.startInstances({
+                InstanceIds: [id]
+            }).promise()
+            return true
+        } catch {
+            return false
+        }
+    },
+    terminateInstance: async (key, id) => {
+        AWS.config.update(key)
+        let ec2 = new AWS.EC2({ apiVersion: '2016-11-15' })
+        try {
+            await ec2.terminateInstances({
+                InstanceIds: [id]
+            }).promise()
+            return true
+        } catch {
+            return false
+        }
+    },
+    runInstances: async (key, args) => {
+        AWS.config.update(key)
+        let ec2 = new AWS.EC2({ apiVersion: '2016-11-15' })
+        try {
+            await ec2.runInstances(args).promise()
+            return true
+        } catch {
+            return false
+        }
+    },
+    modifyInstanceAttribute: async (key, id, args) => {
+        AWS.config.update(key)
+        let ec2 = new AWS.EC2({ apiVersion: '2016-11-15' })
+        try {
+            await ec2.modifyInstanceAttribute({
+                InstanceId: id,
+                ...args
+            }).promise()
+            return true
+        } catch {
+            return false
+        }    
+    }
 }
