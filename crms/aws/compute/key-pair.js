2648a04e698800978d9f0b1c2b95ba818ae45348
Nevation <j209446@naver.com>
2020-08-14 01:01:56 +0900

    Change Test Code

--- crms/aws/compute/key-pair.js
+++ crms/aws/compute/key-pair.js
@@ -5,6 +5,17 @@ module.exports = {
         AWS.config.update(key)
         let ec2 = new AWS.EC2({ apiVersion: '2016-11-15' })
         return await ec2.describeKeyPairs().promise()
+    },
+
+    deleteKeyPair: async (key, id) => {
+        try {
+            AWS.config.update(key)
+            let ec2 = new AWS.EC2({ apiVersion: '2016-11-15' })
+            await ec2.deleteKeyPair({KeyPairId: id}).promise()
+            return true
+        } catch {
+            return false
+        }
     }
 }
 
--- crms/aws/index.js
+++ crms/aws/index.js
@@ -1,5 +1,12 @@
 
-//module.exports = 
+module.exports = {
+    ...require('./compute'),
+    ...require('./network'),
+    ...require('./database'),
+    ...require('./storage')
+}
+
+/*
 let func = {
     ...require('./compute'),
     ...require('./network'),
@@ -8,9 +15,12 @@ let func = {
 }
 
 async function d(){
+    const AWS = require('aws-sdk')
     let key = {"accessKeyId":"AKIATOLCGVDHV56GC2C4","secretAccessKey":"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","region":"ap-northeast-2"}
-    let a = await func.describeSecurityGroups(key)
-    console.log(Object.keys(func))
+    AWS.config.update(key)
+    let a = await func.describeSubnets(key)
+    console.log(a)
 }
 
 d()
+*/
