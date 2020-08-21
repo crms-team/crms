32d98abc3a380681039058683a99dace34730455
LeeYu <reasonz@daum.net>
2020-08-21 09:33:52 +0900

    fixed Create ListView Page (#13)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React,{Component} from 'react';
 import Board from './Components/Board';
 import './App.css';
 import Detail from './Components/Detail/Detail';
+import Listview from './Components/Listview/Listview';
+import EnhancedTable from './Components/Listview/Table';
 
 class App extends Component{
   render(){
     return(
       <div className="App">
         {/* <Board/> */}
-        <Detail />
+        {/* <Detail /> */}
+        <Listview />
       </div>
     );
   }
