3542f7177f6932931314357240835381d53bd92b
reason <reasonz@daum.net>
2020-09-03 04:11:39 +0900

    Add detail page

--- src/components/detail/index.jsx
+++ src/components/detail/index.jsx
@@ -7,8 +7,11 @@ import "./detail.scss";
 const tabName = ["Information", "Object View"];
 
 const thStyle = { width: "46%", "text-align": "right", paddingRight: "4%" };
-const tdStyle = { width: "46%", "text-align": "left", paddingRight: "4%" };
-
+const tdStyle = {
+    width: "46%",
+    "text-align": "left",
+    paddingRight: "4%",
+};
 
 class ListTable extends Component {
     constructor(props) {
@@ -18,35 +21,35 @@ class ListTable extends Component {
     render() {
         let data = this.props.data;
         let key = [];
-        
+
         if (data.length > 0) {
             key = Object.keys(data[0]);
             return (
-                <table style={{
-                    width: "100%",
-                    wordWrap: "break-word",
-                    tableLayout: "fixed",
-                }}>
-                    <tr>
-                        {key.map((v) => {
-                            return <th>{v}</th>;
-                        })}
-                    </tr>
+                <div className="left--table-container">
+                    <table className="detail__right--table">
+                        <tr>
+                            {key.map((v) => {
+                                return <th>{v}</th>;
+                            })}
+                        </tr>
 
-                    {data.map((v) => {
-                        return (
-                            <tr>
-                                {key.map((kv) => {
-                                    let type = typeof(v[kv])
-                                    if (type == "object"){
-                                        return <td>{JSON.stringify(v[kv])}</td>
-                                    }
-                                    return <td>{v[kv]}</td>;
-                                })}
-                            </tr>
-                        );
-                    })}
-                </table>
+                        {data.map((v) => {
+                            return (
+                                <tr>
+                                    {key.map((kv) => {
+                                        let type = typeof v[kv];
+                                        if (type == "object") {
+                                            return (
+                                                <td>{JSON.stringify(v[kv])}</td>
+                                            );
+                                        }
+                                        return <td>{v[kv]}</td>;
+                                    })}
+                                </tr>
+                            );
+                        })}
+                    </table>
+                </div>
             );
         } else {
             return <div>There is no data to display</div>;
@@ -69,17 +72,8 @@ class ObjectView extends Component {
         let type = "length" in data;
 
         return (
-            <div
-                style={{
-                    width: "39%",
-                    height: "98%",
-                    padding: "1%",
-                    position: 'fixed',
-                    right: 0,
-                    marginRight: '4%'
-                }}
-            >
-                {key}
+            <div className="right--table-container">
+                <span className="right--table-title">{key}</span>
                 <hr />
                 {type && <ListTable data={data} />}
             </div>
@@ -94,7 +88,7 @@ class ContentSummary extends Component {
             data: [],
             rootData: [],
 
-            resource: '',
+            resource: "",
             keyList: [],
 
             objectKey: "",
@@ -106,127 +100,134 @@ class ContentSummary extends Component {
 
     async componentDidMount() {
         let response = await (await fetch(this.props.endpoint)).json();
-        let resource = this.props.resource
-        let data = response.data
+        let resource = this.props.resource;
+        let data = response.data;
 
-        if (resource == 'ec2') data = data['Instances'][0]
+        if (resource == "ec2") data = data["Instances"][0];
 
-        this.setState({ 
+        this.setState({
             data: data,
-            rootData: data, 
-            keyList: [resource]
+            rootData: data,
+            keyList: [resource],
         });
     }
 
-    getViewData(keyList){
-        let data = this.state.rootData
+    getViewData(keyList) {
+        let data = this.state.rootData;
 
         for (let i = 1; i < keyList.length; i++) {
-            data = data[keyList[i]]
+            data = data[keyList[i]];
         }
 
-        return data
+        return data;
     }
 
     clickEvent(key, data, type) {
-        if (type == 'list') {
+        if (type == "list") {
             this.setState({
                 objectKey: key,
                 objectData: data,
-            });    
+            });
         } else {
-            let keyList = this.state.keyList
-            keyList.push(key)
+            let keyList = this.state.keyList;
+            keyList.push(key);
             this.setState({
-                objectKey: '',
+                objectKey: "",
                 objectData: [],
                 keyList: keyList,
-                data: this.getViewData(keyList)
-            });    
-    
+                data: this.getViewData(keyList),
+            });
         }
     }
 
     linkClickEvent(idx) {
-        let keyList = this.state.keyList
-        keyList = keyList.slice(0, idx)
+        let keyList = this.state.keyList;
+        keyList = keyList.slice(0, idx);
         this.setState({
             keyList: keyList,
-            data: this.getViewData(keyList)
-        })
+            data: this.getViewData(keyList),
+        });
     }
 
     render() {
         let data = this.state.data;
         let keys = Object.keys(data);
-        let keyList = this.state.keyList
+        let keyList = this.state.keyList;
 
         return (
             <>
-                <div style={{width: "100%", height: "5%"}}>
-                     {
-                        keyList.map((v, idx) => {
-                            return <span onClick={()=>this.linkClickEvent(idx + 1)}> / <span style={{color: '#ffc14d'}}>{v}</span></span>
-                        })
-                    }
+                <div>
+                    {keyList.map((v, idx) => {
+                        return (
+                            <span onClick={() => this.linkClickEvent(idx + 1)}>
+                                {" "}
+                                /{" "}
+                                <span
+                                    className="top-title"
+                                    style={{
+                                        color: "#ffc14d",
+                                        cursor: "pointer",
+                                    }}
+                                >
+                                    {v}
+                                </span>
+                            </span>
+                        );
+                    })}
                 </div>
-                <div style={{ width: "100%", float: "left", marginBottom: "20px" }}>
-                    {   
-                        keys.length != 0 && 
-                        <table
-                        style={{
-                            width: "48%",
-                            wordWrap: "break-word",
-                            tableLayout: "fixed",
-                            float: 'left',
-                            margin: '1%'
-                        }}
-                    >
-                        {keys.map((val, idx) => {
-                            let type = typeof data[val];
-                            
-                            if (data[val] != null && type == "object") {
-                                let Testtype =
-                                    "length" in data[val] ? "list" : "json";
-                                return (
-                                    <tr>
-                                        <th style={thStyle}>{val}</th>
-                                        <td style={tdStyle}>
-                                            <div
-                                             style={{
-                                                 color: '#ffc14d'
-                                             }}
-                                                onClick={() => {
-                                                    this.clickEvent(
-                                                        val,
-                                                        data[val],
-                                                        Testtype
-                                                    );
-                                                }}
-                                            >
-                                                view Object
-                                            </div>
-                                        </td>
-                                    </tr>
-                                );
-                            } else {
-                                return (
-                                    <tr>
-                                        <th style={thStyle}>{val}</th>{" "}
-                                        <td style={tdStyle}>
-                                            { data[val] != null ?data[val].toString() : "null"}
-                                        </td>
-                                    </tr>
-                                );
-                            }
-                        })}
-                    </table>
-                    }
-                    { keys.length == 0 && <div>There is no data to display</div> }
-                <ObjectView
-                    objectKey={this.state.objectKey}
-                    objectData={this.state.objectData}
-                />
+                <hr className="hr" />
+                <div className="contents-container">
+                    {keys.length != 0 && (
+                        <table className="detail__left--table">
+                            {keys.map((val, idx) => {
+                                let type = typeof data[val];
+
+                                if (data[val] != null && type == "object") {
+                                    let Testtype =
+                                        "length" in data[val] ? "list" : "json";
+                                    return (
+                                        <tr>
+                                            <th style={thStyle}>{val}</th>
+                                            <td style={tdStyle}>
+                                                <div
+                                                    style={{
+                                                        color: "#ffc14d",
+                                                        cursor: "pointer",
+                                                    }}
+                                                    onClick={() => {
+                                                        this.clickEvent(
+                                                            val,
+                                                            data[val],
+                                                            Testtype
+                                                        );
+                                                    }}
+                                                    className="view-object"
+                                                >
+                                                    view Object
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    );
+                                } else {
+                                    return (
+                                        <tr>
+                                            <th style={thStyle}>{val}</th>{" "}
+                                            <td style={tdStyle}>
+                                                {data[val] != null
+                                                    ? data[val].toString()
+                                                    : "null"}
+                                            </td>
+                                        </tr>
+                                    );
+                                }
+                            })}
+                        </table>
+                    )}
+                    {keys.length == 0 && <div>There is no data to display</div>}
+                    <ObjectView
+                        objectKey={this.state.objectKey}
+                        objectData={this.state.objectData}
+                    />
                 </div>
             </>
         );
@@ -295,7 +296,10 @@ class Detail extends Component {
                         style={{ overflowY: "scroll" }}
                     >
                         <div className="tab-content">
-                            <ContentSummary endpoint={this.state.endpoint} resource={this.state.resource}/>
+                            <ContentSummary
+                                endpoint={this.state.endpoint}
+                                resource={this.state.resource}
+                            />
                         </div>
                     </div>
                 </div>
