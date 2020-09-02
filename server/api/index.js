95fd6d4cbc355285ef5b9a223ca2c4b450eebb2f
Nevation <j209446@naver.com>
2020-09-02 18:43:49 +0900

    Add API Docs (Close #62)

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,10 @@
 const passwd = require('./passwd')
 const cloud = require('./cloud')
 const dashboard = require('./dashboard')
+const swagger = require('./swagger')
 
 module.exports = server => {
+    swagger(server)
     passwd(server)
     dashboard(server)
     cloud(server)
--- server/api/swagger.js
+++ server/api/swagger.js
@@ -1,7 +1,3 @@
-// init crms
-const system = require('./system')
-const path = require('path')
-const fs = require('fs')
 const swaggerJSDoc = require('swagger-jsdoc')
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = swaggerJSDoc({
@@ -823,55 +819,6 @@ const swaggerSpec = swaggerJSDoc({
   apis: []
 });
 
-
-const PATH = __dirname.split(path.sep).slice(0, -1).join(path.sep)
-
-if (!system.config.existConfig(PATH)){
-    if (!system.config.createConfig(PATH)){
-        console.log("Failed Create Config File path ./data/crms.config")
-        process.exit()
-    }
-}
-
-if (!system.key.existsKeyData(PATH)) {
-    if (!system.key.createKeyData(PATH)){
-        console.log("Failed Create Key Data File path ./data/key_data.json")
-        process.exit()
-    }
-}
-
-// create express server
-const express = require('express')
-const bodyParser = require('body-parser')
-const server = express()
-const cors = require('cors');
-const { Router } = require('react-router-dom')
-
-// add server.config
-server.config = system.config.getConfig(PATH)   
-const PORT = server.config.server_port
-server.keys = require('./system').key
-
-// logging
-//system.log.logger(server.config.path)
-
-server.use(cors())
-server.use(bodyParser.json())
-server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-require('./api')(server)
-
-server.listen(PORT, ()=>{
-    console.log('>> Start Node Server 0.0.0.0:' + PORT)
-})
-
-// using build react 
-if (fs.existsSync(__dirname + '/../build')){
-    server.use(express.static(path.join(__dirname, '../build')))
-    const router = express.Router()
-
-    router.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../build/index.html'))
-    })
-
-    server.use(router)
+module.exports = server => {
+    server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
