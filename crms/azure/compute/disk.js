9e3a0105d0cac4f1c8c9691e6b6c5522c9da141f
backtion <37172677+backtion@users.noreply.github.com>
2020-10-26 04:59:56 +0900

    Modify api version

--- crms/azure/compute/disk.js
+++ crms/azure/compute/disk.js
@@ -17,7 +17,7 @@ async function disks(key, args = undefined) {
 
 async function deleteDisks(key, args = undefined) {
     let token = await getToken(key)
-    let ep = `https://management.azure.com/subscriptions/${key.subscription}/resourceGroups/${args.resourceGroupName}/providers/Microsoft.Compute/disks/${args.name}?api-version=2020-06-01`
+    let ep = `https://management.azure.com/subscriptions/${key.subscription}/resourceGroups/${args.resourceGroupName}/providers/Microsoft.Compute/disks/${args.name}?api-version=2020-06-30`
 
     let res = await fetch(ep, {
         headers: {
@@ -26,7 +26,8 @@ async function deleteDisks(key, args = undefined) {
         },
         method: 'DELETE'
     })
-
+    console.log("res:", res)
+    console.log("args", args)
     return res.status < 400
 }
 
